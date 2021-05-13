c1e212ae7644a381a03dc0259cfd610addca59e0
Philipp Weißmann <github@philipp-weissmann.de>
2021-05-13 21:10:02 +0200

    Update words.js

--- words.js
+++ words.js
@@ -1,38 +1,39 @@
 const words = `
 
 
-Game changer
-Disruptive technology
-Bilateral
-Bandwidth
-Transparency
-Strategic fit
-Core competencies
-Out of the box
-Bottom line
-Revisit
-Take that off-line
-24/7
-Out of the loop
-Benchmark
-Value-added
-Thought leader
-Proactive
-Win-win
-Fast track
-Result-driven
-Empower (or empowerment)
-Knowledge base
-At the end of the day
-Touch base
-Mindset
-Client focus(ed)
-Ballpark
-Game plan
-Leverage
-Cascade
-Sequential or sequentially
-Multiple times
+Gesche singt bei den Vengaboys mit
+Jemand spricht, ist aber stumm geschaltet
+Bei Sven gibt es Nüsschen
+Begeisterung über Frau Hund
+Jemands agt "Da regt mich (allein) schon die Frage auf."
+Jemand ist plötzlich weg - oder doppelt da
+Irgendwas mit Jörn und Döner
+Jemand fragt nach dem Link zum CyTube
+Sascha ist nur kurz Bier holen.
+Ein Wortspiel wird als "auf Blathering-Niveau" eingestuft
+Podstock, Deichpod, Bunkerpod, o.Ä. wird erwähnt
+"Sagt das Mädchen zum Matrosen" oder Variante
+Jemand sagt "Bisamratte!" oder Thomann wird erwähnt
+"Hallöchen!"
+Jitsi Begrüßungs-Winke-Schleife
+Gejammer wegen irgendwas mit Ohrwürmern und nach 21 Uhr.
+"Gute nase!"
+"Prösterchen"
+"Der gebende Blick" oder erwähnt oder gegeben.
+Irgendwer betont (k)eine Hose an zu haben.
+Sven ist mal kurz weg, Jenny kommt nach Hause.
+Jemand setzt ein Plüschtier vor die Kamera.
+Gesche oder Stefan gehen eine Rauchen.
+"Das wird auf gar keinen Fall eskalieren"
+Es gibt neuen Content für Philipps Flachwitze Repo
+"Wo ist eigentlich ...?"
+"Joa... und sonst so?" / "Haben wir noch Themen?"
+Jemand läuft mit dem Endgerät durch die Gegen oder sitzt im ÖPNV
+"Willst Du die Antwort alphabetisch oder nach ICD-10 Nummern?"
+"Auf Wiederjörn"/ "Auf Verasehn" / "Inge gerne gerne"
+"Grüße!"
+"Zwinki Zwonki"
+Jemand strickt, häkelt oder spinnt.
 
 
 `;
